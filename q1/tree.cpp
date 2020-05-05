1baa7119b99d09872deae732030f0787fee943d5
Yanni Chau <ytc19@ic.ac.uk>
2020-05-05 10:03:46 +0800

    T1.1_tree_height_completed

--- q1/tree.cpp
+++ q1/tree.cpp
@@ -20,10 +20,20 @@ int tree_size(const tree_node *node)
     }
 }
 
-int tree_height(const tree_node *node)
-{
-    // TODO:
-    return 0;
+int tree_height(const tree_node *node) //height is distance to bottom-most node.
+{   
+    if (node == nullptr) {
+        return 0;
+    }
+    
+    if ((node->left == nullptr)&&(node->right == nullptr)) {
+        return 0;
+    }
+
+    int l_height = tree_height(node->left) + 1;
+    int r_height = tree_height(node->right) + 1;
+
+    return max(l_height, r_height);
 }
 
 float tree_balance(const tree_node *node)
