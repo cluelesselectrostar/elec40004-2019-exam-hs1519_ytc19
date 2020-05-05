6bbc2d5446bef664f3f3b07d838b76eaad34ba41
Yanni Chau <ytc19@ic.ac.uk>
2020-05-05 13:39:49 +0800

    visualisation_scripts_updates

--- q1/visualise_equal_balance.sh
+++ q1/visualise_equal_balance.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-echo "alpha bravo charlie delta echo" | ./render_as_dot EqualBalanceStringSet >  render_as_dot_equal_balance.render_as_dot
+echo "alpha bravo charlie delta echo" | ./render_as_dot EqualBalanceStringSet >  render_as_dot_equal_balance.dot
 dot render_as_dot_equal_balance.dot -Tpng -o render_as_dot_equal_balance.png
-cat render_as_dot_equal_balance.dot | -Tsvg >render_as_dot_equal_balance.svg
+cat render_as_dot_equal_balance.dot | dot -Tsvg >render_as_dot_equal_balance.svg
 
 
--- q1/visualise_random_balance.sh
+++ q1/visualise_random_balance.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-echo "alpha bravo charlie delta echo" | ./render_as_dot RandomBalanceStringSet >  render_as_dot_random_balance.render_as_dot
+echo "alpha bravo charlie delta echo" | ./render_as_dot RandomBalanceStringSet >  render_as_dot_random_balance.dot
 dot render_as_dot_random_balance.dot -Tpng -o render_as_dot_random_balance.png
-cat render_as_dot_random_balance.dot | -Tsvg >render_as_dot_random_balance.svg
+cat render_as_dot_random_balance.dot | dot -Tsvg >render_as_dot_random_balance.svg
 
 
